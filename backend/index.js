b8e62ff5972e57a909b75602a08788c2a5e86c75
raza1315 <razasfs2003@gmail.com>
2025-03-08 00:18:29 +0530

    image v3

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 
 const express = require("express");
 const cors=require("cors");
+const os =require("os")
 const app = express();
 app.listen(4001,()=>{
 console.log("running on port 4001");
 })
 app.get("/",(req,res)=>{
-res.status(200).json("home route...")
+res.status(200).json(`home route...${os.hostname()}`)
 })
